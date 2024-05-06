4e26841be66c1ffced4bb114400ec31253566290
AKHIL KUMAR <165399470+AKHILKUMARBALIVADA@users.noreply.github.com>
2024-05-06 15:06:45 +0530

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,6 +1,6 @@
 echo "This is GITHUB and Jenkins Integration"
-echo "Looks Cool!"
+echo "Looks great!"
 echo "test is done"
-echo "anil made change in github and code is pushed"
+echo "lahari made change in github and code is pushed"
 echo "This is Github Jenkins Webhooks test"
 
